a5d8f7fd7983e1cc15a57f28f072ba2b46a834b6
Thomas Prescott <tcprescott@gmail.com>
2019-03-06 08:18:19 -0600

    couple fixes

--- spoilerbot/eggs.py
+++ spoilerbot/eggs.py
@@ -1,30 +1,33 @@
 from discord.ext import commands
 
 class Eggs:
+    def __init__(self, bot):
+        self.bot = bot
+
     # bunch of fluff because I like fluff
     @commands.command(hidden=True)
-    async def pizza(ctx):
+    async def pizza(self, ctx):
         await ctx.send('🍕')
 
     @commands.command(hidden=True)
-    async def beer(ctx):
+    async def beer(self, ctx):
         await ctx.send('🍺')
 
     @commands.command(hidden=True)
-    async def dead(ctx):
+    async def dead(self, ctx):
         await ctx.send('⚰️')
 
     @commands.command(hidden=True)
-    async def mudora(ctx):
+    async def mudora(self, ctx):
         await ctx.send('<:mudora:536293302689857567>')
 
     @commands.command(hidden=True)
-    async def linkface(ctx):
+    async def linkface(self, ctx):
         await ctx.send('<:LinkFace:545809659651686400>')
 
     @commands.command(hidden=True)
     @commands.has_any_role('admin')
-    async def throwerror(ctx):
+    async def throwerror(self, ctx):
         raise Exception
 
 def setup(bot):
